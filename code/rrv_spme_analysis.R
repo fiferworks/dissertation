e0c943a825571f27cb40e3d3622adc4a917eab07
fiferworks <fiferworks@gmail.com>
2021-08-05 14:39:47 -0400

    beginning to do a PCA

--- code/rrv_spme_analysis.R
+++ code/rrv_spme_analysis.R
@@ -1,3 +1,4 @@
+####SETUP####
 pkgs <-
   c("tidyverse",
     "readxl",
@@ -13,12 +14,11 @@ lapply(pkgs, library, character.only = TRUE)
 rm(pkgs, nu_pkgs)
 
 # reading in master datasheet
-df <- read_excel("data/rrd_spme_master_datasheet.xlsx", col_types = "guess")
+df <-
+  read_excel("data/ofv_pca_table.xlsx", col_types = "guess")
 
-# changing remaining columns to factors
-df <- df %>%
-  mutate_if(is.character, as.factor)
 
-####FIGURE OUT  HOW TO PIVOT WIDER ON THE SAMPLE NAME!!!####
 
-df %>% pivot_wider(id_cols = Sample, names_from = `Peak Name`, values_from = `IS Relative Area (%)`)
+ 
+# #cleanup
+# rm(list = ls())
