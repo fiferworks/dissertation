0c87889b4ab37da93477f23cf590887160f02cf5
fiferworks <fiferworks@gmail.com>
2021-08-09 11:41:21 -0400

    added umap packages and implementation

--- code/rrv_spme_analysis_umap.R
+++ code/rrv_spme_analysis_umap.R
@@ -1,7 +1,8 @@
 ####SETUP####
 pkgs <-
   c("tidyverse",
-    "factoextra",
+    "umap",
+    "Rcpp",
     "ggplot2",
     "Cairo",
     "readxl",
@@ -21,100 +22,56 @@ df <-
   read_excel("data/ofv_pca_table.xlsx", col_types = "guess")
 
 
-####PRINCIPAL COMPONENT ANALYSIS####
-pca <- df %>% select(-Sample,-Treatment) %>%
-  prcomp(center = TRUE, scale = FALSE)
+####UNIFORM MANIFOLD APPROXIMATION AND PROJECTION####
 
-# summary(pca)
-#
-# # can be interpreted as Pearson correlation coefficients
-# map_dfc(1:27, ~pca$rotation[,.]*sqrt(pca$sdev^2)[.])
-# # the following code does the same things with the factoextra package
-# pca_dat$coord
+spme_umap <- select(df,-Sample,-Treatment) %>%
+  as.matrix() %>%
+  umap(
+    n_neighbors = 7,
+    min_dist = 0.1,
+    metric = "manhattan",
+    n_epochs = 200,
+    verbose = TRUE
+  )
 
-pca_dat <- get_pca(pca)
-
-####PLOTTING THE PCA VALUES####
-fviz_pca_biplot(pca, label = 'var')
-
-#saving the file
-ggsave(
-  'figure/spme_biplot_graph.png',
-  plot = last_plot(),
-  type = 'cairo',
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
-
-# fviz_pca_var(pca)
-# fviz_screeplot(pca, addlabels = TRUE, choice = "eigenvalue")
-
-fviz_screeplot(pca, addlabels = TRUE, choice = "variance")
-
-#saving the file
-ggsave(
-  'figure/spme_screeplot_graph.png',
-  plot = last_plot(),
-  type = 'cairo',
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
-
-# plotting the PCAs against one another
-spme_pca <- df %>%
-  mutate(PCA1 = pca$x[, 1], PCA2 = pca$x[, 2])
-
-ggplot(spme_pca,
-       aes(
-         PCA1,
-         PCA2,
-         color = Treatment,
-         shape = Treatment,
-         label = Treatment
-       )) +
-  geom_point(size = 2, alpha = 0.6) +
-  theme_bw() +
-  stat_ellipse(level = 0.95)
-
-#saving the file
-ggsave(
-  'figure/spme_pca_graph.png',
-  plot = last_plot(),
-  type = 'cairo',
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
-
-ggplot(spme_pca,
-       aes(
-         PCA1,
-         PCA2,
-         color = Treatment,
-         shape = Treatment,
-         label = Treatment
-       )) +
-  geom_point(size = 2, alpha = 0.6) +
-  theme_bw() +
-  stat_ellipse(level = 0.95) +
-  geom_text(aes(label = Sample), hjust = -0.05, vjust = 0)
-
-#saving the file
-ggsave(
-  'figure/spme_pca_labeled_graph.png',
-  plot = last_plot(),
-  type = 'cairo',
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
-
-
-#cleanup
-rm(list = ls())
+# #saving the file
+# ggsave(
+#   'figure/spme_screeplot_graph.png',
+#   plot = last_plot(),
+#   type = 'cairo',
+#   width = 16,
+#   height = 9,
+#   scale = 1,
+#   dpi = 300
+# )
+# 
+# # plotting the PCAs against one another
+# spme_pca <- df %>%
+#   mutate(PCA1 = pca$x[, 1], PCA2 = pca$x[, 2])
+# 
+# ggplot(spme_pca,
+#        aes(
+#          PCA1,
+#          PCA2,
+#          color = Treatment,
+#          shape = Treatment,
+#          label = Treatment
+#        )) +
+#   geom_point(size = 2, alpha = 0.6) +
+#   theme_bw() +
+#   stat_ellipse(level = 0.95)
+# 
+# #saving the file
+# ggsave(
+#   'figure/spme_pca_graph.png',
+#   plot = last_plot(),
+#   type = 'cairo',
+#   width = 16,
+#   height = 9,
+#   scale = 1,
+#   dpi = 300
+# )
+# 
+# 
+# #cleanup
+# rm(list = ls())
