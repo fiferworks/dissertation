a49b2ce1ce78cfd2ee29955563340045f476d6e7
fiferworks <fiferworks@gmail.com>
2021-09-02 12:37:35 -0400

    working through tutorials

--- code/impact_network_analysis.R
+++ code/impact_network_analysis.R
@@ -1,22 +1,6 @@
 ####IMPACT THROUGH NETWORKS####
 # a list of packages used
-pkgs <-
-  c(
-    'tidyverse',
-    'sf',
-    'ggmap',
-    'ggplot2',
-    'ggthemes',
-    'extrafont',
-    'showtext',
-    'leaflet',
-    'leafsync',
-    'dplyr',
-    'readxl',
-    'mapview',
-    'htmlwidgets',
-    'viridis'
-  )
+pkgs <- c('dplyr', 'igraph')
 
 # checks to see if you have the packages installed,
 # if you don't, it will try to install them
@@ -24,8 +8,56 @@ nu_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
 if (length(nu_pkgs))
   install.packages(nu_pkgs)
 
-# #you need to install phantomjs for the interactive maps
-# webshot::install_phantomjs()
-
-#loading required packages
+#loading packages from pkgs
 lapply(pkgs, library, character.only = TRUE)
+
+
+mat1 <-
+  matrix(c(0, 1, 0, 0, 0, 1, 1, 0, 0), nrow = 3, ncol = 3) # matrix function
+
+mat2 <- graph_from_adjacency_matrix(mat1)
+
+plot(mat2, edge.arrow.size = 1)
+
+mat3 <-
+  graph(edge = c(1, 3, 3, 2, 2, 1),
+        n = 3,
+        directed = TRUE) # use graph function and list edges
+plot(
+  mat3,
+  edge.arrow.size = 1,
+  vertex.color = 'purple',
+  vertex.size = 20
+)
+
+mtgraph <- make_empty_graph(50)
+plot(
+  mtgraph,
+  vertex.size = 1 ,
+  vertex.label = NA,
+  vertex.color = "plum"
+)
+
+fulgraph <- make_full_graph(50)
+plot(
+  fulgraph,
+  vertex.size = 1 ,
+  vertex.label = NA,
+  vertex.color = "plum"
+)
+
+tregraph <- make_tree(50)
+plot(
+  tregraph,
+  vertex.size = 1 ,
+  vertex.label = NA,
+  vertex.color = "plum"
+)
+
+erdograph <- sample_gnm(n = 100, m = 40)
+plot(
+  erdograph,
+  vertex.size = 1 ,
+  vertex.label = NA,
+  vertex.color = "plum"
+)
