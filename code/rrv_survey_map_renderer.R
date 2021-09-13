796ff2c40210e05bd51fbcdb891efda77ebbdf8f
fiferworks <fiferworks@gmail.com>
2021-09-13 18:00:34 -0400

    developed new maps for the survey data, small multiples to show the progression of mite invasion

--- code/rrv_survey_map_renderer.R
+++ code/rrv_survey_map_renderer.R
@@ -6,13 +6,14 @@ pkgs <-
     'sf',
     'ggplot2',
     'ggthemes',
-    'ggmap',
+    'tmap',
     'extrafont',
     'showtext',
     'leaflet',
     'leafsync',
     'dplyr',
     'mapview',
+    'naniar',
     'htmlwidgets',
     'viridisLite',
     'viridis'
@@ -78,14 +79,15 @@ df_fl <- df %>% filter(df$state == 'FL')
 df <-
   st_as_sf(df,
            coords = c("lon", "lat"),
-           crs = 4326)
+           crs = 4326) # data was collected by phone GPS, should be using WGS84
 
+# fgb isn't supported right now
 mapviewOptions(fgb = FALSE)
 
 map_pf_mites <-
   mapview(
     df,
-    col.regions = c("#00326FFF", "gray50"),
+    col.regions = c("yellow", "gray50"),
     homebutton = FALSE,
     cex = 'eriophyoids',
     na.color = c("gray50"),
@@ -108,7 +110,7 @@ df_fl <-
 map_mites_fl <-
   mapview(
     df_fl,
-    col.regions = c("#00326FFF", "gray50"),
+    col.regions = c("yellow", "gray50"),
     homebutton = FALSE,
     cex = 'eriophyoids',
     na.color = c("gray50"),
@@ -142,240 +144,63 @@ map_other_mites_fl <-
 #saves the map
 mapshot(map_other_mites_fl, file = 'figure/rrv_survey_map_fl_other.png')
 
-
-# ####COMPARING MITE HOTSPOTS OVER TIME#####
-# #subsetting the data for different months to compare trends
-# fl_jan <- filter(df_fl, month == 'January')
-# fl_feb <- filter(df_fl, month == 'February')
-# fl_mar <- filter(df_fl, month == 'March')
-# fl_apr <- filter(df_fl, month == 'April')
-# fl_may <- filter(df_fl, month == 'May')
-# fl_jun <- filter(df_fl, month == 'June')
-# fl_jul <- filter(df_fl, month == 'July')
-# fl_aug <- filter(df_fl, month == 'August')
-# fl_sep <- filter(df_fl, month == 'September')
-# fl_oct <- filter(df_fl, month == 'October')
-# fl_nov <- filter(df_fl, month == 'November')
-# fl_dec <- filter(df_fl, month == 'December')
-#
-# #map for february
-# m_feb <-
-#   mapview(
-#     fl_feb,
-#     col.regions = c("#00326FFF", "gray50"),
-#     alpha.regions = 0.8,
-#     zcol = 'p_fructiphilus',
-#     layer.name = 'P. fructiphilus present: February',
-#     map.types = 'CartoDB.Positron'
-#   )
-#
-# mapshot(m_feb, file = 'figure/map_pf_feb_fl.png')
-#
-# #map for july
-# m_jul <- mapview(
-#   fl_jul,
-#   col.regions = c("#00326FFF", "gray50"),
-#   alpha.regions = 0.8,
-#   zcol = 'p_fructiphilus',
-#   layer.name = 'P. fructiphilus present: July',
-#   map.types = 'CartoDB.Positron'
-# )
-#
-# mapshot(m_jul, file = 'figure/map_pf_jul_fl.png')
-#
-# #a way to look at both maps at the same time
-# leafsync::sync(m_feb, m_jul, ncol = 2, no.initial.sync = F)
-
-###MAPPING WITH GGPLOT####
-# #list of styles: http://leaflet-extras.github.io/leaflet-providers/preview/
-# #maps the sf object with the Stamen Toner Lite style
-# mites_map_toner <- mapview(df_fl, map.types = 'Stamen.TonerLite')
-# mapshot(mites_map_toner, file = 'p_fruct_map_fl_toner.png')
-
-# #colors the points by number of mites found
-# map_den_pf_fl <-
-#   mapview(df_fl,
-#           cex = 'eriophyoids',
-#           zcol = 'eriophyoids',
-#           map.types = 'Stamen.TonerLite')
-# #saves the map
-# mapshot(map_den_pf_fl, file = '../images/map_den_pf_fl.png')
-
-
-####GGPLOT2 METHODS####
+####TMAP METHODS####
 #reads in the shapefile for Florida
 fl <- st_read('data/Florida_Counties.shp')
-# leon <- filter(fl, COUNTYNAME == 'LEON')
-city <-
-  st_read('data/City_Limits_for_Tallahassee_Florida.shp')
 
-#plots points on the map
-map_gg_pf_fl <-
-  ggplot() +
-  geom_sf(data = fl) +
-  geom_sf(
-    data = df_fl,
-    aes(color = p_fructiphilus, cex = eriophyoids),
-    show.legend = T
-  ) +
-  scale_color_viridis_d(
-    na.value = 'gray50',
-    option = 'E',
-    begin = 0.1,
-    end = 0.5
-  ) +
-  theme_tufte(base_size = 30, base_family = "gill_sans") +
-  ggtitle(expression(
-    'Sites Surveyed for ' ~ italic(P.fructiphilus) ~ ' in Florida 2017-2021'
-  )) +
-  theme(axis.title = element_blank(),  axis.text.x = element_blank()) +
-  theme(
-    plot.title = element_text(
-      size = 100,
-      face = "bold",
-      family = "garamond"
-    ),
-    axis.text.x = element_text(
-      color = "grey20",
-      size = 50,
-      angle = 0,
-      hjust = .5,
-      vjust = .5,
-      face = "plain"
-    ),
-    axis.text.y = element_text(
-      color = "grey20",
-      size = 50,
-      angle = 0,
-      hjust = 1,
-      vjust = 0,
-      face = "bold"
-    )
-  )
-ggsave(
-  'figure/rrv_survey_map_gg_pf_fl.png',
-  plot = last_plot(),
-  type = "cairo",
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
+#read in data for small multiples maps:
+fl_locs <- read_csv("data/rrv_survey_fl_table.csv")
+fl_locs %>% filter(fl_locs$erios > 0)
+fl_locs <- st_as_sf(fl_locs,
+                    coords = c("lon", "lat"),
+                    crs = 4326)
+#making vector into factors and reordering so 'Yes' appears first in the legend:
+fl_locs$'Eriophyoids' <- fl_locs$pfruct
+fl_locs$Eriophyoids <-
+  ifelse(fl_locs$Eriophyoids == TRUE, "Yes", "No")
+fl_locs$Eriophyoids <- as_factor(fl_locs$Eriophyoids)
 
-#plots points on the map
-map_gg_other_fl <-
-  ggplot() +
-  geom_sf(data = fl) +
-  geom_sf(data = df_fl,
-          aes(cex = other_mites),
-          show.legend = T) +
-  scale_color_viridis_d(
-    na.value = 'gray50',
-    option = 'E',
-    begin = 0.1,
-    end = 1
-  ) +
-  theme_tufte(base_size = 30, base_family = "gill_sans") +
-  ggtitle(expression(
-    'Other mites found with ' ~ italic(P.fructiphilus) ~ ' in Florida 2017-2021'
-  )) +
-  theme(axis.title = element_blank(),  axis.text.x = element_blank()) +
-  theme(
-    plot.title = element_text(
-      size = 100,
-      face = "bold",
-      family = "garamond"
-    ),
-    axis.text.x = element_text(
-      color = "grey20",
-      size = 50,
-      angle = 0,
-      hjust = .5,
-      vjust = .5,
-      face = "plain"
-    ),
-    axis.text.y = element_text(
-      color = "grey20",
-      size = 50,
-      angle = 0,
-      hjust = 1,
-      vjust = 0,
-      face = "bold"
-    )
-  )
+#plotting the places where we found erios
+fl_map_erios <-
+  tm_shape(fl) + tm_fill() + tm_borders() +
+  tm_shape(fl_locs %>% filter(year != 2017)) +
+  tm_dots("Eriophyoids",
+          palette = viridis(
+            2,
+            alpha = 1,
+            begin = 1,
+            end = 0.5,
+            direction = 1,
+            option = "E"
+          )) +
+  tm_facets(by = 'year',
+            ncol = 2,
+            free.coords = FALSE) + tm_scale_bar(position = 'left') + tm_layout(
+              main.title.size = 3.5,
+              fontfamily = "gill_sans",
+              main.title = 'Eriophyoid mites found on roses in northern Florida',
+              main.title.position = "left"
+            )
 
-ggsave(
-  'figure/rrv_survey_map_gg_other_fl.png',
-  plot = last_plot(),
-  type = "cairo",
-  width = 16,
-  height = 9,
-  scale = 1,
-  dpi = 300
-)
+tmap_save(fl_map_erios, scale = 3, filename = "figure/rrv_survey_map_years_pf.png")
 
-# ####GGMAP VERSION####
-# chr <-
-#   get_map('312 Stadium Dr, Tallahassee, FL 32304',
-#           maptype = "satellite",
-#           zoom = 16)
-#
-# png(filename = 'figure/ggmap_pf_city_FSU.png',
-#     width = 1280,
-#     height = 1280)
-# ggmap(chr) +
-#   geom_sf(data = df_fl,
-#           aes(cex = eriophyoids, color = eriophyoids),
-#           inherit.aes = F) +
-#   scale_color_viridis(option = 'C') +
-#   coord_sf(crs = st_crs(4326)) +
-#   labs(x = "Longitude",
-#        y = "Latitude",
-#        title = "Population of P. fructiphilus in Tallahassee") +
-#   theme(
-#     plot.title = element_text(
-#       size = 25,
-#       face = "bold",
-#       hjust = 0.1,
-#       color = "grey20",
-#       family = "garamond"
-#     ),
-#     axis.text.x = element_text(
-#       color = "grey20",
-#       size = 20,
-#       angle = 0,
-#       hjust = .5,
-#       vjust = .5,
-#       face = "plain"
-#     ),
-#     axis.text.y = element_text(
-#       color = "grey20",
-#       size = 20,
-#       angle = 0,
-#       hjust = 1,
-#       vjust = 0,
-#       face = "bold"
-#     ),
-#     axis.title.x = element_text(
-#       color = "grey20",
-#       size = 20,
-#       angle = 0,
-#       hjust = 0.5,
-#       vjust = 0,
-#       face = "bold"
-#     ),
-#     axis.title.y = element_text(
-#       color = "grey20",
-#       size = 20,
-#       angle = 0,
-#       hjust = 0,
-#       vjust = 0.5,
-#       face = "bold"
-#     ),
-#     legend.title = element_text(color = "grey20", size = 18),
-#     legend.text = element_text(color = "grey20", size = 17)
-#   )
-# dev.off()
+#plotting the places where we found other mites
+fl_map_other <-
+  tm_shape(fl) + tm_fill() + tm_borders() +
+  tm_shape(fl_locs %>% filter(year != 2017)) +
+  tm_dots('other',
+          palette = 'viridis') +
+  tm_facets(by = 'year',
+            ncol = 2,
+            free.coords = FALSE) + tm_scale_bar(position = 'left') + tm_layout(
+              main.title.size = 3.5,
+              fontfamily = 'gill_sans',
+              main.title = 'Other mites found on roses in northern Florida',
+              main.title.position = "left"
+            )
+
+tmap_save(fl_map_other, scale = 3, filename = "figure/rrv_survey_map_years_others.png")
+
 
 ####CLEANUP####
 rm(list = ls())
