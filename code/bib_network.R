1784b9d3fd18043722dfcf0a58d3f338b2c96686
fiferworks <fiferworks@gmail.com>
2021-09-28 12:07:53 -0400

    I thought it might be interesting to look at my citation network

---
+++ code/bib_network.R
@@ -0,0 +1,14 @@
+####SETUP####
+pkgs <- c('bib2df')
+
+# installs missing packages
+nu_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+if (length(nu_pkgs))
+  install.packages(nu_pkgs)
+
+# loading required packages
+lapply(pkgs, library, character.only = TRUE)
+rm(pkgs, nu_pkgs)
+
+df <- bib2df(file = 'bib/r_citations.bib')
+
