e8fa0e664a1e154ef2ce88c907e100df064ff2b0
fiferworks <fiferworks@gmail.com>
2021-10-29 01:26:05 -0400

    working on a way to do generalized linear hypotheses on the zip model, but it has to be done manually, and I don't know what I am doing...

--- code/rrv_actigard_analysis.R
+++ code/rrv_actigard_analysis.R
@@ -1,5 +1,12 @@
 ####SETUP####
-pkgs <- c('lme4', 'pscl', 'car', 'boot', 'multcomp', 'lubridate', 'tidyverse')
+pkgs <-
+  c('lme4',
+    'pscl',
+    'car',
+    'boot',
+    'multcomp',
+    'lubridate',
+    'tidyverse')
 
 # installs missing packages
 nu_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
@@ -12,54 +19,30 @@ rm(pkgs, nu_pkgs)
 
 ####ACTIGARD DATA ANALYSIS####
 #reading in the data
-df <- read_csv('data/rrv_actigard_master_datasheet.csv', col_types = "fdffff?")
+df <-
+  read_csv('data/rrv_actigard_master_datasheet.csv', col_types = "ffddfffdfdd?")
+
+grifn <- df %>% filter(Field == 'Griffin')
 
 #data is now ready to be analyzed
-zip_model <- zeroinfl(Mites ~ Treatment, data = df, dist = "poisson", link ='logit')
+####ZIP MODEL OF GRIFFIN DATA####
+zip_model <-
+  zeroinfl(
+    `Total P.fructiphilus` ~ Treatment,
+    data = grifn,
+    dist = "poisson",
+    link = 'logit'
+  )
 
 summary(zip_model)
 Anova(zip_model, type = c("III")) #treatment is significant
 
-# dput(coef(zip_model, "count"))
-# dput(coef(zip_model, "zero"))
-# 
-# f <- function(data, i) {
-#   require(pscl)
-#   m <- zeroinfl(Mites ~ Treatment,
-#                 data = data[i,],
-#                 start = list(
-#                   count = c(
-#                     2.7850328037862,
-#                     -7.58605319814307,
-#                     0.339553706088603,
-#                     -0.103990370364982,
-#                     -3.86373629052554e-09
-#                   ),
-#                   zero = c(
-#                     2.379547947819,
-#                     -7.55230340935205,
-#                     0.888141120188275,
-#                     0.564874029193537,
-#                     15.1891111024181
-#                   )
-#                 ))
-#   as.vector(t(do.call(rbind, coef(summary(
-#     m
-#   )))[, 1:2]))
-# }
-# 
-# set.seed(5)
-# res <- boot(df, f, R = 1200, parallel = "snow", ncpus = 8)
-# res
-# 
-# parms <- t(sapply(c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19), function(i) {
-#   out <- boot.ci(res, index = c(i, i + 1), type = c("perc", "bca"))
-#   with(out, c(Est = t0, pLL = percent[4], pUL = percent[5],
-#               bcaLL = bca[4], bcaLL = bca[5]))
-# }))
-
 #glmer model,  Model is nearly unidentifiable: large eigenvalue ratio
-glm_1 <- glmer(Mites ~ Treatment + (1 | Block), family = 'poisson', data = df)
+glm_1 <-
+  glmer(`Total P.fructiphilus` ~ Treatment + (1 |
+                                                Block),
+        family = 'poisson',
+        data = grifn)
 
 summary(glm_1)
 
@@ -67,8 +50,50 @@ summary(glm_1)
 Anova(glm_1, type = c("III")) #treatment is significant
 
 #multiple comparisons
-#suggests that both the low Actigard rate and Kontos treatments are significantly different than the water treatment
+#glmer model suggests that both the low Actigard rate and Kontos treatments are significantly different than the water treatment
 summary(glht(glm_1, linfct = mcp(Treatment = "Tukey")), test = adjusted("holm"))
+
+
+#compute contrasts "by hand" for ZIP model: https://hypatia.math.ethz.ch/pipermail/r-help/2009-March/418679.html
+nr <- length(levels(grifn$Treatment)) + 1
+contr <- matrix(0, nrow = nr, ncol = length(coef(zip_model)))
+colnames(contr) <- names(coef(zip_model))
+rownames(contr) <-
+  paste(levels(grifn$Treatment)[c(2, 2, 3, 3, 5, 5)],
+        levels(grifn$Treatment)[c(1, 3, 5, 1, 2, 1)], sep = " - ")
+contr[, 2:4] <-
+  contrMat(numeric(nrow(contr)), type = "Tukey")[7:12, 2:4]
+glht_zip_model <- glht(zip_model, linfct = contr)
+summary(glht_zip_model)
+
+
+# data("NMES1988", package = "AER")
+# nmes <- NMES1988[, c(1, 6:8)]
+#
+# ## models
+# fm_pois <- glm(visits ~ ., data = nmes, family = poisson)
+# fm_nbin <- glm.nb(visits ~ ., data = nmes)
+# fm_zinb <- zeroinfl(visits ~ . | ., data = nmes, dist = "negbin")
+#
+# ## generalized linear hypotheses
+# glht_pois <- glht(fm_pois, linfct = mcp(health = "Tukey"))
+# glht_nbin <- glht(fm_nbin, linfct = mcp(health = "Tukey"))
+#
+# ## compute contrasts "by hand" for ZINB
+# nr1 <- length(levels(nmes$health))
+# contr1 <- matrix(0, nrow = nr1, ncol = length(coef(fm_zinb)))
+# colnames(contr1) <- names(coef(fm_zinb))
+# rownames(contr1) <- paste(levels(nmes$health)[c(2, 3, 3)],
+#                           levels(nmes$health)[c(1, 1, 2)], sep = " - ")
+# contr1[, 3:4] <-
+#   contrMat(numeric(nrow(contr1)), type = "Tukey")[, -2]
+# glht_zinb <- glht(fm_zinb, linfct = contr1)
+#
+# ## multiple comparisons
+# summary(glht_pois)
+# summary(glht_nbin)
+# summary(glht_zinb)
+
 
 #making a compact letter display for each treatment
 q <- glht(glm_1, linfct = mcp(Treatment = "Tukey"))
